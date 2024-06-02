8e6cd573dad0d0c7cbde4929c931e15686bbeb9a
orlandomarin <orlandomarin@orlandos-air.lan>
2024-06-02 11:36:34 -0400

    Third Python Program - Check if number is positive, negative, or zero

---
+++ main.py
@@ -0,0 +1,15 @@
+# Define a number 10 and then use if-else statements to check whether it is
+# positive, negative, or zero. The output will be "The number is positive."
+# since 10 is greater than 0.
+
+# define number variable and set it equal to 10
+number = 10
+
+# check if the  number is positive, negative,  or zero and print the result
+
+if (number > 0):
+    print("The number is positive!")
+elif (number < 0):
+    print("The number is negatiive!")
+else:
+    print("The number is 0!")
